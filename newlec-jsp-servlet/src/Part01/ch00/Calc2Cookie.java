b36e15ca3f95901c562485cf8ae260cc8e53e4b6
wcjeong <wcjeong@hist.co.kr>
2021-04-16 15:39:56 +0900

    4월 16일 첫번째

--- newlec-jsp-servlet/src/Part01/ch00/Calc2Cookie.java
+++ newlec-jsp-servlet/src/Part01/ch00/Calc2Cookie.java
@@ -6,18 +6,20 @@ import java.io.PrintWriter;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 
-@WebServlet("/calc2")
-public class Calc2 extends HttpServlet {
+@WebServlet("/calccookie")
+public class Calc2Cookie extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		ServletContext application = request.getServletContext();
-		HttpSession session = request.getSession();
+//		ServletContext application = request.getServletContext();
+//		HttpSession session = request.getSession();
+		Cookie[] cookies = request.getCookies();
 		
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
@@ -35,10 +37,29 @@ public class Calc2 extends HttpServlet {
 		if(op.equals("=")) {
 			
 			//int x = (Integer)application.getAttribute("value");
-			int x = (Integer)session.getAttribute("value");
+			//int x = (Integer)session.getAttribute("value");
+			
+			int x = 0;
+			for(Cookie c : cookies ) {
+				if(c.getName().equals("value")) {
+					x = Integer.parseInt(c.getValue());
+					break;
+				}	
+			}
+			
+			
 			int y = v;
 			//String operator = (String)application.getAttribute("op");
-			String operator = (String)session.getAttribute("op");
+			//String operator = (String)session.getAttribute("op");
+			
+			String operator="";
+			for(Cookie c : cookies ) {
+				if(c.getName().equals("op")) {
+					operator = c.getValue();
+					break;
+				}	
+			}
+			
 			int result = 0;
 			
 			if(operator.equals("+"))
@@ -52,8 +73,17 @@ public class Calc2 extends HttpServlet {
 		else {
 			//application.setAttribute("value", v);
 			//application.setAttribute("op", op);
-			session.setAttribute("value", v);
-			session.setAttribute("op", op);
+			//session.setAttribute("value", v);
+			//session.setAttribute("op", op);
+			
+			//쿠키에는 문자열만 저장가능
+			Cookie valueCookie = new Cookie("value", String.valueOf(v));
+			Cookie opCookie = new Cookie("op", op);
+			response.addCookie(valueCookie);
+			response.addCookie(opCookie);
+			
+			response.sendRedirect("calc2-cookie.html");
+			
 		}
 	}
 }
